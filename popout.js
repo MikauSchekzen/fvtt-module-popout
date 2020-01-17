ec59b0331ee68d8610bccde390a941bd633d2eee
Tijmen Bok <mikauschekzen@gmail.com>
2020-01-17 14:32:02 +0100

    Update for 0.4.4

--- popout.js
+++ popout.js
@@ -1,41 +1,51 @@
 class PopoutModule {
-	static onRenderJournalSheet(obj, html, data) {
-		let element = html.find(".window-header .window-title");
-		PopoutModule.addPopout(element, `game.journal.get("${obj.entity.id}").sheet`);
-	}
-	static onRenderActorSheet(obj, html, data) {
-		let element = html.find(".window-header .window-title");
-		PopoutModule.addPopout(element, `game.actors.get("${obj.entity.id}").sheet`);
-	}
-	static addPopout(element, sheet) {
-		// Can't find it?
-		if (element.length !== 1) {
-			return;
-		}
-		let popout = $('<a class="popout" style><i class="fas fa-external-link-alt"></i>PopOut!</a>');
-		popout.on('click', (event) => PopoutModule.onPopoutClicked(event, sheet));
-		element.after(popout);
+  static onRenderJournalSheet(obj, html, data) {
+    let element = html.find(".window-header .window-title");
+    PopoutModule.addPopout(
+      element,
+      `game.journal.get("${obj.entity.id}").sheet`
+    );
+  }
+  static onRenderActorSheet(obj, html, data) {
+    let element = html.find(".window-header .window-title");
+    PopoutModule.addPopout(
+      element,
+      `game.actors.get("${obj.entity.id}").sheet`
+    );
+  }
+  static addPopout(element, sheet) {
+    // Can't find it?
+    if (element.length !== 1) {
+      return;
+    }
+    let popout = $(
+      '<a class="popout" style><i class="fas fa-external-link-alt"></i>PopOut!</a>'
+    );
+    popout.on("click", event => PopoutModule.onPopoutClicked(event, sheet));
+    element.after(popout);
+  }
+  static onPopoutClicked(event, sheet) {
+    // Lazy way of finding sheet to close
+    const sheetToClose = eval(sheet);
+    if (sheetToClose != null) sheetToClose.close();
 
-	}
-	static onPopoutClicked(event, sheet) {
-		// Lazy way of finding sheet to close
-		const sheetToClose = eval(sheet);
-		if (sheetToClose != null) sheetToClose.close();
+    let div = $(event.target).closest("div");
+    let window_title = div
+      .find(".window-title")
+      .text()
+      .trim();
 
-		let div = $(event.target).closest("div");
-		let window_title = div.find(".window-title").text().trim();
+    // Create a new html document
+    let html = $("<html>");
+    let head = $("<head>");
+    let body = $("<body>");
 
-		// Create a new html document
-		let html = $("<html>");
-		let head = $("<head>");
-		let body = $("<body>");
-
-		// Copy classes from html/head/body tags and add title
-		html.attr("class", $("html").attr("class"));
-		head.attr("class", $("head").attr("class"));
-		head.append($("<title>" + window_title + "</title>"));
-		body.attr("class", $("body").attr("class"));
-		/*
+    // Copy classes from html/head/body tags and add title
+    html.attr("class", $("html").attr("class"));
+    head.attr("class", $("head").attr("class"));
+    head.append($("<title>" + window_title + "</title>"));
+    body.attr("class", $("body").attr("class"));
+    /*
 		// Clone the journal sheet so we can modify it safely
 		div = div.clone()
 		// Avoid other apps with the same id from destroying this div
@@ -52,28 +62,27 @@ class PopoutModule {
 			"padding": "15px",
 		})
 		body.append(div)*/
-		html.append(head);
-		html.append(body);
+    html.append(head);
+    html.append(body);
 
-		// Copy the scripts and css so the sheet appears correctly
-		for (let link of $("head link")) {
-			let new_link = $(link).clone();
-			// Replace the href with the full URL
-			if (new_link.href !== "")
-				new_link.attr("href", link.href);
-			head.append(new_link);
-		}
-		for (let script of $("head script,body script")) {
-			let new_script = $(script).clone();
-			// Replace the src with the full URL
-			if (script.src !== "")
-				new_script.attr("src", script.src);
-			head.append(new_script);
-		}
-		head.append($("<script>canvas = {};</script>"));
-		// Avoid having the UI initialized which renders the chatlog and all sorts
-		// of other things behind the sheet
-		body.append($(`<script>
+    // Copy the scripts and css so the sheet appears correctly
+    for (let link of $("head link")) {
+      let new_link = $(link).clone();
+      // Replace the href with the full URL
+      if (new_link.href !== "") new_link.attr("href", link.href);
+      head.append(new_link);
+    }
+    for (let script of $("head script,body script")) {
+      let new_script = $(script).clone();
+      // Replace the src with the full URL
+      if (script.src !== "") new_script.attr("src", script.src);
+      head.append(new_script);
+    }
+    head.append($("<script>canvas = {};</script>"));
+    // Avoid having the UI initialized which renders the chatlog and all sorts
+    // of other things behind the sheet
+    body.append(
+      $(`<script>
 		      Game.prototype.initializeUI = function() {
 				ui.nav = new SceneNavigation();
 				ui.controls = new SceneControls();
@@ -81,75 +90,74 @@ class PopoutModule {
 				ui.sidebar = new Sidebar();
 				// sidebar elements only get created on the render
 				// but we don't want to render them
-				ui.chat = new ChatLog();
-				ui.combat = new CombatTracker();
-				ui.scenes = new SceneDirectory();
-				ui.actors = new ActorDirectory();
-				ui.items = new ItemDirectory();
-				ui.journal = new JournalDirectory();
-				ui.tables = new RollTableDirectory();
-				ui.playlists = new PlaylistDirectory();
-				ui.compendium = new CompendiumDirectory();
-				ui.settings = new Settings();
-				ui.players = new PlayerList();
-				ui.pause = new Pause();
-				ui.menu = new MainMenu();
+                                ui.webrtc = new CameraViews(this.webrtc);
+				ui.chat = new ChatLog({ tabName: "chat" });
+				ui.combat = new CombatTracker({ tabName: "combat" });
+				ui.scenes = new SceneDirectory({ tabName: "scenes" });
+				ui.actors = new ActorDirectory({ tabName: "actors" });
+				ui.items = new ItemDirectory({ tabName: "items" });
+				ui.journal = new JournalDirectory({ tabName: "journal" });
+				ui.tables = new RollTableDirectory({ tabName: "tables" });
+				ui.playlists = new PlaylistDirectory({ tabName: "playlists" });
+				ui.compendium = new CompendiumDirectory({ tabName: "compendium" });
+				ui.settings = new Settings({ tabName: "settings" });
 					}
 					Hooks.once('ready', async () => {
 						let forceProceed = false;
-						setTimeout(() => { forceProceed = true; }, 2000);
-						while (${sheet} instanceof ActorSheet && !forceProceed) await (() => { return new Promise(resolve => { setTimeout(() => resolve(), 25); }); })();
+						setTimeout(() => { forceProceed = true; }, 3000);
+						while (${sheet} instanceof ActorSheet && !forceProceed) await (() => { return new Promise(resolve => { setTimeout(() => resolve(), 10); }); })();
 						PopoutModule.renderPopout(${sheet});
 					});
 		      window.dispatchEvent(new Event('load'));
-					</script>`));
-		// Open new window and write the new html document into it
-		// We need to open it to the same url because some images use relative paths
-		let win = window.open(window.location.toString());
-		// console.log(win, window.location)
-		// This is for electron which doesn't have a Window but a BrowserWindowProxy
-		if (win.document === undefined) {
-			win.eval(`document.write(\`<!doctype html>${html[0].outerHTML}\`); document.close();`);
-		} else {
-			win.document.write(`<!doctype html>${html[0].outerHTML}`);
-			// After doing a write, we need to do a document.close() so it finishes
-			// loading and emits the load event.
-			win.document.close();
-		}
-	}
+					</script>`)
+    );
+    // Open new window and write the new html document into it
+    // We need to open it to the same url because some images use relative paths
+    let win = window.open(window.location.toString());
+    // console.log(win, window.location)
+    // This is for electron which doesn't have a Window but a BrowserWindowProxy
+    if (win.document === undefined) {
+      win.eval(
+        `document.write(\`<!doctype html>${html[0].outerHTML}\`); document.close();`
+      );
+    } else {
+      win.document.write(`<!doctype html>${html[0].outerHTML}`);
+      // After doing a write, we need to do a document.close() so it finishes
+      // loading and emits the load event.
+      win.document.close();
+    }
+  }
 
-	static renderPopout(sheet) {
-		sheet._original_popout_render = sheet._render;
-		sheet.options.minimizable = false;
-		sheet.options.resizable = false;
-		sheet.options.id = "popout-" + sheet.id;
-		sheet.options.closeOnSubmit = false;
-		Object.defineProperty(sheet, 'id', { value: sheet.options.id, writable: true, configurable: true });
-		// Replace the render function so if it gets re-rendered (such as switching journal view mode), we can
-		// re-maximize it.
-		sheet._render = async function (force, options) {
-			await this._original_popout_render(true, options);
-			// In 0.4.1, there's a bug where a JournalSheet could return before it's done rendering.
-			while (!this._rendered) {
-				await new Promise((resolve) => setTimeout(() => resolve(), 0));
-			}
-			// Maximum it
-			sheet.element.css({ width: "100%", height: "100%", top: "0px", left: "0px" });
-			// Remove the close and popout buttons
-			sheet.element.find("header .close, header .popout").remove();
-		}
-		sheet.render(true);
-	}
+  static renderPopout(sheet) {
+    sheet._original_popout_render = sheet._render;
+    sheet.options.minimizable = false;
+    sheet.options.resizable = false;
+    sheet.options.id = "popout-" + sheet.id;
+    sheet.options.closeOnSubmit = false;
+    Object.defineProperty(sheet, "id", {
+      value: sheet.options.id,
+      writable: true,
+      configurable: true
+    });
+    // Replace the render function so if it gets re-rendered (such as switching journal view mode), we can
+    // re-maximize it.
+    sheet._render = async function(force, options) {
+      await this._original_popout_render(true, options);
+      // Maximize it
+      sheet.element.css({
+        width: "100%",
+        height: "100%",
+        top: "0px",
+        left: "0px"
+      });
+      // Remove the close and popout buttons
+      sheet.element.find("header .close, header .popout").remove();
+    };
+    sheet.render(true);
+  }
 }
 
-Hooks.on('ready', () => {
-	Hooks.on('renderJournalSheet', PopoutModule.onRenderJournalSheet);
-	// Hook to render on any actor sheets
-	let sheets = [];
-	for (let type in CONFIG["Actor"].sheetClasses) {
-		sheets = sheets.concat(Object.values(CONFIG["Actor"].sheetClasses[type]));
-	}
-	for (let sheet of sheets.map(s => s.cls.name).reduce((c, s) => { if (!c.includes(s)) c.push(s); return c; }, [])) {
-		Hooks.on('render' + sheet, PopoutModule.onRenderActorSheet);
-	}
+Hooks.on("ready", () => {
+  Hooks.on("renderJournalSheet", PopoutModule.onRenderJournalSheet);
+  Hooks.on("renderActorSheet", PopoutModule.onRenderActorSheet);
 });
